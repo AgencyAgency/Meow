53f0234237ce48fc147356c49463ba38122451f0
Kyle Oba <koba@pasdechocolat.com>
2013-10-15 11:56:07 -1000

    Initial meow work.

---
+++ Meow/AAViewController.m
@@ -0,0 +1,34 @@
+//
+//  AAViewController.m
+//  Meow
+//
+//  Created by Kyle Oba on 10/15/13.
+//  Copyright (c) 2013 Kyle Oba. All rights reserved.
+//
+
+#import "AAViewController.h"
+
+@interface AAViewController ()
+
+@end
+
+@implementation AAViewController
+
+- (IBAction)kitten1Pressed:(UIButton *)sender
+{
+    NSLog(@"meow");
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
